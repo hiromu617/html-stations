7cd6e17735969ca978fd4b34895b24eb049f224d
hiro <hiromhiro617@gmail.com>
2021-08-12 18:49:57 +0900

    commit

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,35 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affilication: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affilication: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
-}
+function test(userList) {
+  const newUserList = userList.map((data) => {
+    return { ...data, full_name: `${data.family_name} ${data.first_name}` };
+  });
 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(newUserList);
+    }, 3000);
+  });
+
+  return promise;
+}
